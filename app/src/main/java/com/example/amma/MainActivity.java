4153d0a8fda1e19e5403dfd1c91f928a545c1a01
meaomeaoletscode <angleyap0807@gmail.com>
2024-09-04 17:51:42 +0800

    Try the image button

--- app/src/main/java/com/example/amma/MainActivity.java
+++ app/src/main/java/com/example/amma/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -31,15 +32,15 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(binding.appBarMain.toolbar);
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_addAsset, R.id.nav_editAsset, R.id.nav_deleteAsset)
+                R.id.nav_home, R.id.nav_addAsset, R.id.nav_editAsset, R.id.nav_customizeLabel)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
     }
 
     @Override
--- app/src/main/java/com/example/amma/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/amma/ui/home/HomeFragment.java
@@ -1,31 +1,42 @@
 package com.example.amma.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.example.amma.R;
 import com.example.amma.User;
 import com.example.amma.UserManager;
 import com.example.amma.databinding.FragmentHomeBinding;
+import com.example.amma.ui.addasset.AddAssetFragment;
 
 public class HomeFragment extends Fragment {
 
+    private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
     private User currentUser;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
+        final ImageButton btnAddAsset = binding.btnAddAsset;
+        final ImageButton btnEditAsset = binding.btnEditAsset;
+        final ImageButton btnCustomizeLabel = binding.btnCustomizeLabel;
 
         User currentUser = UserManager.getInstance().getCurrentUser();
 
@@ -35,8 +46,55 @@ public class HomeFragment extends Fragment {
             textView.setText("No user logged in");
         }
 
+        btnAddAsset.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
+            navController.navigate(R.id.nav_addAsset);
+        });
+
+        btnEditAsset.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
+            navController.navigate(R.id.nav_editAsset);
+        });
+
+        btnCustomizeLabel.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
+            navController.navigate(R.id.nav_customizeLabel);
+        });
+
+//        btnAddAsset.setOnClickListener(new BtnAddAssetClickListener());
+//        btnEditAsset.setOnClickListener(new BtnEditAssetClickListener());
+//        btnCustomizeLabel.setOnClickListener(new BtnCustomizeLabelClickListener());
+
         return root;
     }
+
+    public class BtnAddAssetClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
+            navController.navigate(R.id.nav_addAsset);
+        }
+    }
+
+    public class BtnEditAssetClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
+            navController.navigate(R.id.nav_editAsset);
+        }
+    }
+
+    public class BtnCustomizeLabelClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
+            navController.navigate(R.id.nav_customizeLabel);
+        }
+    }
+
 
     @Override
     public void onDestroyView() {
